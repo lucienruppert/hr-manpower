e59ffc12722d2ead78d19bfff0c4b427db9405e0
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-11-28 16:09:10 +0100

    change base_url

---
+++ .angular/cache/19.0.1/frontend/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-ONLG5MDZ.js";
+import "./chunk-TSG4KAW3.js";
+import "./chunk-SJO2QKKA.js";
+import "./chunk-AJMJUZ6M.js";
+import "./chunk-5TID76VL.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: "https://luciendelmar.com/backend_hr/api",
+  BASE_URL: "https://luciendelmar.com/backend/api/hr",
 };
