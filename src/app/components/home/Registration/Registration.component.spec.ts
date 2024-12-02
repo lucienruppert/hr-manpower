d24b11f0019b57f0326ec4c427a50c7cd57de3fc
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2024-12-02 14:30:56 +0100

    finx angular bundle size warning

--- src/app/components/home/Registration/Registration.component.spec.ts
+++ src/app/components/home/Registration/Registration.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { RegistrationComponent } from './Registration.component';
+import { RegistrationComponent } from './registration.component';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
